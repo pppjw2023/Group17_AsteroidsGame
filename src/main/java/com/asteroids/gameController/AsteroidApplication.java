5904921044b9b98b991bcde289218976d103aafe
pppjw2023 <123629358+pppjw2023@users.noreply.github.com>
2023-04-20 02:00:03 +0100

    Add files via upload

--- src/main/java/com/asteroids/gameController/AsteroidApplication.java
+++ src/main/java/com/asteroids/gameController/AsteroidApplication.java
@@ -1,10 +1,12 @@
 package com.asteroids.gameController;
-
 import com.asteroids.gameObjects.GameObject;
 import javafx.application.Application;
+import javafx.scene.media.Media;
 import javafx.stage.Stage;
+import javafx.scene.media.MediaPlayer;
 
 
+import java.io.File;
 import java.io.IOException;
 import java.util.*;
 
@@ -22,17 +24,34 @@ public class AsteroidApplication extends Application {
 
     public static int CurrentScore = 0;
 
+    // add BGM
+    private static final String BGM_PATH = System.getProperty("user.dir") + File.separator + "BGM.mp3";
+    private static final String BACKGROUND_MUSIC = BGM_PATH;
+
+    private MediaPlayer mediaPlayer;
+
 
     @Override
     public void start(Stage stage) throws IOException {
+        // play BGM
+        Media backgroundMusic = new Media(new File(BACKGROUND_MUSIC).toURI().toString());
+        mediaPlayer = new MediaPlayer(backgroundMusic);
+        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+        mediaPlayer.play();
+
         startAsteroidsApp(stage);
     }
- 
+
 
     public static void startAsteroidsApp(Stage stage) {
         AsteroidScenes.showMenu(stage);
     }
 
+    @Override
+    public void stop() throws Exception {
+        // stop BGM when exit app
+        mediaPlayer.stop();
+    }
 
     public static void main(String[] args) {
         launch();
